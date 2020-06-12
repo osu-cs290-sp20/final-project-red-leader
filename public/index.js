a395d25500bbb8740f0541159602d68f39d8dd4c
ThisExistsNow <dane767@hotmail.com>
2020-06-12 13:03:26 -0700

    lets see what we got here

---
+++ public/index.js
@@ -0,0 +1,9 @@
+function showjournal() {
+    var journalfate = document.getElementById('journal-container');
+    journalfate.classList.remove('hidden');
+}
+
+function hidejournal() {
+    var journalfate = document.getElementById('journal-container');
+    journalfate.classList.add('hidden');
+}
---
+++ server.js
@@ -0,0 +1,49 @@
+var path = require('path');
+var express = require('express');
+var exphbs = require('express-handlebars');
+
+var posts = require('./posts');
+var pixels = require('./pixel');
+var digis = require('./digital');
+//var analogs = require('./physicals');
+//var signups = require('./emails');
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'views/art')));
+
+//important bizz goes here
+
+app.get('/', function(req, res) {
+  res.status(200).render('home');
+});
+
+app.get('/artblog', function(req, res) {
+  res.status(200).render('artblog', {
+    entries: posts
+  });
+});
+
+app.get('/comission', function(req, res) {
+  res.status(200).render('comission', {
+    vgs : pixels,
+    digiarts : digis
+  });
+});
+
+app.get('/newsletter', function(req, res) {
+  res.status(200).render('newsletter')
+});
+
+app.get('*', function (req, res) {
+  res.status(404).render('404');
+});
+
+app.listen(port, function () {
+  console.log("== Server is listening on port", port);
+});
